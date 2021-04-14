e64fce7fde6adf2d85f3fa826a7184af1104f60c
buyngkwonc <byungkwonc@gmail.com>
2021-04-14 21:02:47 +0900

    pandas까지 연습

--- wikidocs.py
+++ wikidocs.py
@@ -1,23 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
- 
-def get_per(code):
-	url = "http://finance.naver.com/item/main.nhn?code=" + code
-   	html = requests.get(url).text
+from pandas import Series, DataFrame
 
-   	soup = BeautifulSoup(html, "html5lib")
-   	tags = soup.select("#_per")
-   	tag = tags[0]
-   	return float(tag.text)
-
-def get_dividend(code):
-   	url = "http://finance.naver.com/item/main.nhn?code=" + code
-   	html = requests.get(url).text
-
-   	soup = BeautifulSoup(html, "html5lib")
-   	tags = soup.select("#_dvr")
-   	tag = tags[0]
-   	return float(tag.text)
-
-print(get_per("000660"))
-print(get_dividend("000660"))
+data = {"open": [737, 750], "high": [755, 780], "low": [700, 710], "close": [750, 770]} 
+df = DataFrame(data) 
+s = Series([300, 400])  #DataFrame에 추가할 Series 객체를 생성
+df["volume"] = s        #DataFrame에 volume이란 이름으로 Series 객체를 추가
+print(df)
+df.loc[2] = (100, 200, 300, 400, 500)   #행을 추가할 때는 loc를 사용해서 행의 이름과 데이터를 튜플 혹은 리스트로 넘겨주면 됩
+print(df)
+upper = df["open"] * 1.3
+df["upper"] = upper
+print(df)
