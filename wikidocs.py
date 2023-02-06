bcf4b2bc92c5f66910d776f65201646eb0a1c5ec
최병권 <qudrnjs@hiconsy.com>
2023-02-06 18:18:08 +0900

    신규 빌드

--- wikidocs.py
+++ wikidocs.py
@@ -5,10 +5,10 @@ con_key = "90634f4f04183c3d5fbb3960566df585"
 sec_key = "b60e48d7b12d41725bc9b5506670a7b3"
 
 bithumb = pybithumb.Bithumb(con_key, sec_key)
-for ticker in pybithumb.get_tickers() :
-    balance = bithumb.get_balance(ticker)
-    print(ticker, ":", format(balance[0],'f'))
-    time.sleep(0.1)
+#for ticker in pybithumb.get_tickers() :
+#    balance = bithumb.get_balance(ticker)
+#    print(ticker, ":", format(balance[0],'f'))
+#    time.sleep(0.1)
 
 bcha = bithumb.get_balance("BCHA")
 ltc = bithumb.get_balance("LTC")
@@ -16,9 +16,10 @@ xrp = bithumb.get_balance("XRP")
 bsv = bithumb.get_balance("BSV")
 etc = bithumb.get_balance("ETC")
 bch = bithumb.get_balance("BCH")
-print("비트코인 캐시 에이비씨", format(bcha, 'f'))
+
+print("비트코인 캐시 에이비씨", bcha)
 print("라이트코인", ltc)
-print("리픓", xrp)
+print("리플", xrp)
 print("비트코인네스브이", bsv)
 print("이더리움 클래식", etc)
 print("비트코인 캐시", bch)
